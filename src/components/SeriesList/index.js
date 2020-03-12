fec3d6a8a21473d93d0f43bd38443c8a64b14ecb
Simon <shwaldemar@gmail.com>
2020-03-12 20:45:42 +0000

    shows api fetched and components added for series series-list and container for fetch tvshow api

---
+++ src/components/SeriesList/index.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import Series from '../../containers/Series';
+import './index.css';
+
+const SeriesListItem = ({ series }) => (
+    <li key={series.show.id}>
+      {series.show.name}
+    </li>
+)
+
+const SeriesList = (props) => {
+  return (
+    <div>
+      <ul className="series-list">
+      {props.list.map(series => {
+        return (
+          <SeriesListItem series={ series } />
+          )
+      })}
+      </ul>
+    </div>
+  )
+}
+
+export default SeriesList;
